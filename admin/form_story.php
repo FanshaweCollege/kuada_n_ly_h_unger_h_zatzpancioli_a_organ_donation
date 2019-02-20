e3712b1cce34491e1f2781ddb74daef4a59611e8
annapancioli <annapancioli@gmail.com>
2019-02-19 22:10:35 -0500

    started developing share your story form

---
+++ admin/form_story.php
@@ -0,0 +1,43 @@
+<?php
+	require_once('scripts/config.php');
+    
+    if(isset($_POST['submit'])){
+        $name = trim($_POST['name']);
+        $email = trim($_POST['email']);
+        $story = trim($_POST['story']);
+        if(empty($name) || empty($story)){
+            $message = 'Please fill the required fields';
+        }else{
+            $result = sendStory($name,$email,$story);
+            $message = $result;
+        }
+    }
+?>
+
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Create User</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type='text/css' media="screen" href="../css/main.css">
+    <script src="../js/main.js"></script>
+</head>
+<body>
+    <?php if(!empty($message)):?>
+		<p><?php echo $message;?></p>
+	<?php endif?>
+    <h2>Share your story</h2>
+    <form action="form_story.php" method="POST">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" placeholder="Your name"><br><br>
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" placeholder="Your email"><br><br>
+        <label for="story">Story:</label>
+        <textarea type="text" id="story" name="story" placeholder="Your story"></textarea><br><br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+        
+</body>
+</html>
---
+++ admin/scripts/send_story.php
@@ -0,0 +1,53 @@
+<?php
+
+function sendStory($name,$email,$story){
+
+    include('connect.php');
+
+    $check_name_query = "SELECT COUNT(*) FROM tbl_story WHERE story_name = :sname";
+	$check_name_exist = $pdo->prepare($check_name_query );
+	$check_name_exist->execute(
+	array(
+		':sname'=> $name
+	)
+    );
+    
+    if(($check_name_exist->fetchColumn()) > 0){
+		$message = "Already sent your story!";
+		return $message;
+    }
+    
+    $create_story_query = 'INSERT INTO tbl_story(story_name,story_email,story_subject)';
+	$create_story_query .= ' VALUES(:sname,:email,:story)';
+	$create_story_set = $pdo->prepare($create_story_query);
+	$create_story_set->execute(
+		array(
+			':sname'=>$name,
+            ':email'=>$email,
+            ':story'=>$story
+		)
+    );
+    
+    if($create_story_set->rowCount()){
+
+		$message = 'Thank you for sending us your story!';
+		redirect_to('form_story.php');
+		
+    }else{
+      $message = "Story couldn't be sent!";
+      return $message;
+    }
+
+
+    /*$to = 'admin@lifegoeson.com';
+    $subject = "'.$name.' shared a story";
+    $message = 
+    'Name: '.$name.'<br>
+    Email: '.$email.'<br>
+    Story: '.$story.';'
+
+    mail($to,$subject,$message);*/
+      
+  }
+    
+?>
