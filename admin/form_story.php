ca34fcac296a381d92b97cb403b7685bd0b82b77
annapancioli <annapancioli@gmail.com>
2019-02-21 00:11:19 -0500

    fixed share your story errors

--- admin/form_story.php
+++ admin/form_story.php
@@ -21,7 +21,7 @@
 <head>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Create User</title>
+    <title>Share Your Story</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type='text/css' media="screen" href="../css/main.css">
     <script src="../js/main.js"></script>
--- admin/scripts/send_story.php
+++ admin/scripts/send_story.php
@@ -4,20 +4,21 @@ function sendStory($name,$email,$story){
 
     include('connect.php');
 
-    $check_name_query = "SELECT COUNT(*) FROM tbl_story WHERE story_name = :sname";
-	$check_name_exist = $pdo->prepare($check_name_query );
-	$check_name_exist->execute(
+  $check_info_query = "SELECT COUNT(*) FROM tbl_story WHERE story_name = :sname AND story_email = :email";
+	$check_info_exist = $pdo->prepare($check_info_query );
+	$check_info_exist->execute(
 	array(
-		':sname'=> $name
+    ':sname'=> $name,
+    ':email'=> $email
 	)
     );
     
-    if(($check_name_exist->fetchColumn()) > 0){
+    if(($check_info_exist->fetchColumn()) > 0){
 		$message = "Already sent your story!";
 		return $message;
     }
     
-    $create_story_query = 'INSERT INTO tbl_story(story_name,story_email,story_subject)';
+  $create_story_query = 'INSERT INTO tbl_story(story_name,story_email,story_subject)';
 	$create_story_query .= ' VALUES(:sname,:email,:story)';
 	$create_story_set = $pdo->prepare($create_story_query);
 	$create_story_set->execute(
@@ -31,7 +32,7 @@ function sendStory($name,$email,$story){
     if($create_story_set->rowCount()){
 
 		$message = 'Thank you for sending us your story!';
-		redirect_to('form_story.php');
+		return $message;
 		
     }else{
       $message = "Story couldn't be sent!";
