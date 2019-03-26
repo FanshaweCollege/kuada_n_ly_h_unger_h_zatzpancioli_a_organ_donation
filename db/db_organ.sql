21a9bd5ef5c09fbbe849051c1e20f33b886cfe59
Helen Unger <35934535+HelenUnger@users.noreply.github.com>
2019-03-25 20:38:21 -0400

    updated database for stories

--- db/db_organ.sql
+++ db/db_organ.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 28, 2019 at 06:19 AM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Generation Time: Mar 26, 2019 at 12:37 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -52,20 +52,21 @@ INSERT INTO `tbl_event` (`event_id`, `event_img`, `event_description`) VALUES
 DROP TABLE IF EXISTS `tbl_story`;
 CREATE TABLE IF NOT EXISTS `tbl_story` (
   `story_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `story_name` varchar(75) CHARACTER SET utf8 NOT NULL,
-  `story_email` varchar(75) CHARACTER SET utf8 NOT NULL,
-  `story_reason` varchar(75) CHARACTER SET utf8 NOT NULL,
-  `story_subject` text CHARACTER SET utf8 NOT NULL,
+  `story_title` varchar(75) CHARACTER SET utf8 NOT NULL,
+  `story_author` varchar(75) CHARACTER SET utf8 NOT NULL,
+  `story_img` varchar(75) CHARACTER SET utf8 NOT NULL DEFAULT 'default-story.jpg',
+  `story_text` text CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`story_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_story`
 --
 
-INSERT INTO `tbl_story` (`story_id`, `story_name`, `story_email`, `story_reason`, `story_subject`) VALUES
-(1, 'test', 'test@gmail.com', 'Share your story', 'test'),
-(2, 'test2', 'test2@gmail.com', 'Questions about donation', 'test2');
+INSERT INTO `tbl_story` (`story_id`, `story_title`, `story_author`, `story_img`, `story_text`) VALUES
+(1, 'One Story', 'Joey Yo', 'joey.jpg', 'story one blah blah blah'),
+(2, 'Story Two', 'Monica', 'monica.jpg', 'story 2 blah blah blah'),
+(5, 'tester', 'meeeee', 'pink.jpg', 'this is the story!');
 
 -- --------------------------------------------------------
 
@@ -83,14 +84,16 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `user_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_ip` varchar(50) NOT NULL DEFAULT 'no',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_name`, `user_pass`, `user_email`, `user_date`, `user_ip`) VALUES
-(7, 'Admin', 'admin', 'password', 'admin@admin.com', '2019-02-28 05:36:32', '::1');
+(7, '', '', '', '', '2019-03-25 21:50:28', '::1'),
+(8, 'Helen', 'MasterBuilder', '$2y$10$VZRhd/5bSxiFbLkDsbmvt.8jRqtV7pV2Z4mZ0swJnC8BWTMFdy7YS', 'master@builder.ca', '2019-03-25 21:47:23', 'no'),
+(9, 'd', 'd', '$2y$10$WYINxuLTCc57hFl6eeXX8e..zawkPztbxihS.qyMWYybMnWbjhGSG', 'dake@dake.ca', '2019-03-25 21:49:34', 'no');
 
 -- --------------------------------------------------------
 
